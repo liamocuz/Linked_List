40c37811a57501ba0b2cb92fe01dd84a93a92ffb
liamocuz <liamo@me.com>
2021-01-01 21:24:32 -0500

    Refactored, added LinkedList class

--- node.hpp
+++ node.hpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+
 class Node {
 public:
     int data;
@@ -13,5 +16,48 @@ public:
         data = data;
         next = nullptr;
     }
+};
+class LinkedList {
+private:
+    Node head;
 
+public:
+    LinkedList()
+    {
+        head = Node();
+    }
+
+    LinkedList(int length)
+    {
+        head = Node();
+        Node* temp = &head;
+        for (int i = 0; i < length; i++)
+        {
+            temp->next = &Node();
+            temp = temp->next;
+        }
+    }
+
+    LinkedList(std::vector<int> arr)
+    {
+        Node* temp = &head;
+        int size = arr.size();
+        for (int i = 0; i < size; i++)
+        {
+            temp = &Node(arr[i]);
+            temp = temp->next;
+        }
+    }
+
+    void printList()
+    {
+        Node* tmp = &head;
+
+        while (tmp != nullptr)
+        {
+            std::cout << tmp->data << ", ";
+            tmp = tmp->next;
+        }
+        std::cout << std::endl;
+    }
 };
