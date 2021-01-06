d4395453fe214c1238d1562d29680be5fce457df
liamocuz <liamo@me.com>
2021-01-06 17:15:09 -0500

    Added ability to add arrays into list

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 int main (int argc, char** argv)
 {
-    std::vector<int> arr {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> arr1 {1,2,3,4,5,6,7,8,9,10};
 
-    LinkedList list = LinkedList(arr);
-    list.reverse();
+    int arr2[10] = {2,4,6,8,10,12,14,16,18,20};
+
+    LinkedList list = LinkedList(5);
+    list.addArrToPos(arr2, 10, 3);
     list.print();
 
     return EXIT_SUCCESS;
--- node.hpp
+++ node.hpp
@@ -21,24 +21,60 @@ class LinkedList {
 private:
     Node* head;
     Node* tail;
+    int length;
+
+    struct NodeTuple {
+        Node* front;
+        Node* back;
+    };
+
+    template <typename Array>
+    NodeTuple createNodesFromArray(const Array arr, const int len)
+    {
+        Node* front = new Node(arr[0]);
+        Node* back = nullptr;
+        Node* temp = front;
+        for (int i = 1; i < len; i++)
+        {
+            temp->next = new Node(arr[i]);
+            temp = temp->next;
+        }
+        back = temp;
+
+        NodeTuple tuple;
+        tuple.front = front;
+        tuple.back = back;
+
+        return tuple;
+    }
 
 public:
     LinkedList()
     {
-        head = nullptr;
-        tail = nullptr;
+        head = new Node();
+        tail = head;
+        length = 1;
     }
 
-    LinkedList(const int length)
+    LinkedList(const int len)
     {
+        if (len < 1)
+        {
+            std::cout << "Cannot create a list of length < 1." << std::endl;
+            return;
+        }
+
         head = new Node();
         Node* temp = head;
-        for (int i = 0; i < length; i++)
+        int i = 1;
+        while (i < len)
         {
             temp->next = new Node();
             temp = temp->next;
+            i++;
         }
         tail = temp;
+        length = len;
     }
 
     LinkedList(const std::vector<int> arr)
@@ -46,12 +82,14 @@ public:
         head = new Node(arr[0]);
         Node* temp = head;
         int size = arr.size();
-        for (int i = 1; i < size; i++)
+        int i = 1;
+        for (i = 1; i < size; i++)
         {
             temp->next = new Node(arr[i]);
             temp = temp->next;
         }
         tail = temp;
+        length = i;
     }
 
     void addToFront(const int val)
@@ -59,6 +97,7 @@ public:
         Node* newNode = new Node(val);
         newNode->next = head;
         head = newNode;
+        length++;
         return;
     }
 
@@ -73,6 +112,75 @@ public:
         Node* newNode = new Node(val);
         tail->next = newNode;
         newNode->next = nullptr;
+        length++;
+        return;
+    }
+
+    void addToPos(const int val, const int pos)
+    {
+        // pos is 0 indexed
+        if (pos > length)
+        {
+            std::cout << "The length of the list is shorter than the input position." << std::endl;
+            return;
+        }
+        Node* newNode = new Node(val);
+        Node* temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        return;
+    }
+
+    template <typename Array>
+    void addArrToBack(const Array arr, const int len)
+    {
+        NodeTuple tuple = createNodesFromArray(arr, len);
+        tail->next = tuple.front;
+        tail = tuple.back;
+        length += len;
+
+        return;
+    }
+
+    template <typename Array>
+    void addArrToFront(const Array arr, const int len)
+    {
+        NodeTuple tuple = createNodesFromArray(arr, len);
+        tuple.back->next = head;
+        head = tuple.front;
+        length += len;
+
+        return;
+    }
+
+    template <typename Array>
+    void addArrToPos(const Array arr, const int len, const int pos)
+    {
+        NodeTuple tuple = createNodesFromArray(arr, len);
+        // pos is 0 indexed
+        if (pos > length)
+        {
+            std::cout << "The length of the list is shorter than the input position." << std::endl;
+            return;
+        }
+
+        Node* temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        tuple.back->next = temp->next;
+        temp->next = tuple.front;
+
+        if (pos == length)
+            tail = tuple.back;
+
+        length += len;
+
         return;
     }
 
@@ -97,6 +205,8 @@ public:
 
     void print()
     {
+        std::cout << "Length of list: " << length << std::endl;
+        
         Node* temp = head;
 
         while (temp != nullptr)
