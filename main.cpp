2ecc3bcc64a1ce454478d7d52e04178444328ac5
liamocuz <liamo@me.com>
2021-03-24 01:11:44 -0400

    Changed

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 int main (int argc, char** argv)
 {
-    std::vector<int> arr1 {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> arr1 = {1,2,3,4,5,6,7,8,9,10};
 
     int arr2[10] = {2,4,6,8,10,12,14,16,18,20};
 
     LinkedList list = LinkedList(5);
     list.addArrToPos(arr2, 10, 3);
+    list.reverse();
     list.print();
 
     return EXIT_SUCCESS;
